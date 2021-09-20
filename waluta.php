8ddde8d548c496e70960449291e9cf5894ed8716
wdomeracki <90198583+wdomeracki@users.noreply.github.com>
2021-09-20 18:25:27 +0200

    Add files via upload

---
+++ waluta.php
@@ -0,0 +1,31 @@
+<!-- <?php
+    $e = 2; 
+    define('euro', 4.60);
+    echo '2 euro to ' .$e*euro;
+?> -->
+<!-- <?php
+    $x = 10; 
+    $y = 5; 
+    echo $x+$y. '<br>';
+    echo $x-$y. '<br>';
+    echo $x*$y. '<br>';;
+    echo $x%$y. '<br>';;
+?> -->
+
+<!-- <?php
+    $x = 2; 
+    $y = 3; 
+    $z = 5; 
+    echo $x++. '<br>'; // w nastepnej operacji bedzie 3 ale teraz przy wyswietlaniu jest 2 
+    echo $y--. '<br>'; // w nastepnej operacji bedzie 2 ale teraz przy wyswietlaniu jest 3
+    echo $z += $x; // 5 + 3 bo x juz wynosi 3 
+    echo '<br>';
+    echo --$z; // z = bylo rowne 8 ale z racji ze minusy sa przed to juz przy wyswietlaniu jest 7
+    
+    
+?> -->
+<?php
+    $x = 7;
+    $y = 3;
+    echo ($x + 2 * $y)/2;
+?>
